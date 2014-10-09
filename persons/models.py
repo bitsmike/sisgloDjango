e032d2a5aa83d1fbc015c4c31ad038c07728ac96
Bitsmike <bitsmike@gmail.com>
2014-10-09 16:57:24 -0500

    construccion y maduracion de los modelos personas y generals

--- persons/models.py
+++ persons/models.py
@@ -1,21 +1,17 @@
-#encoding:utf-8
+# -*-encoding:utf-8 -*-
 from django.db import models
-from photos.models import Photo
-from cenpobs.models	import Cenpob
-from distritos.models import Distrito
-from provincia
+from centros_poblados.models import Centro_poblado
 
 # Create your models here.
-class Person(models.Model):
+class Persona(models.Model):
 	dni = models.CharField(max_length=8, blank=True, unique=True)
-	first_name = models.CharField(max_length=60)
-	last_name = models.CharField(max_length=80)
+	nombres = models.CharField(max_length=60)
+	apellidos = models.CharField(max_length=80)
 	fecnac = models.DateField(blank=True)
-	gender = models.CharField(max_length=1)
-	address = models.CharField(max_length=180)
-	neighborhood = models.CharField(max_length=100)
-
-	cell1 = models.CharField(max_length=9, blank=True)
-	cell2 = models.CharField(max_length=9, blank=True)
+	sexo = models.CharField(max_length=1)
+	direccion = models.CharField(max_length=180)
+	barrio = models.CharField(max_length=100)
+	ubicacion = models.Foreignkey(Centro_poblado)
+	celular1 = models.CharField(max_length=9, blank=True)
+	celular2 = models.CharField(max_length=9, blank=True)
 	email = models.EmailField(blank=True)
-	person_photo = models.ForeignKey(Photo, blank=True)
